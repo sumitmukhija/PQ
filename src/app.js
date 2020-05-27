49d64b347e161218d1f08e33b89e454a921d99c1
sumit mukhija <sumukhija@gmail.com>
2020-05-27 13:38:53 +0100

    Add handlebars

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-const app = require('express')();
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.send('working!')
+    res.render('index', {quote: 'Test quote'})
 });
 
 app.listen(PORT)
