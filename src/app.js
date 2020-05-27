ce53c6a3fc88afe853feac8e3784fc3ae38e9578
sumit mukhija <sumukhija@gmail.com>
2020-05-27 14:48:22 +0100

    Add controller with API call, section padding, error handled

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const path = require("path");
+const path = require('path');
 const hbs = require('hbs');
+const quote = require('./controllers/quote_controller')
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -10,7 +11,18 @@ app.set('view engine', 'hbs')
 hbs.registerPartials(path.join(__dirname, '../views/partials'))
 
 app.get('/', (req, res) => {
-    res.render('index', {quote: 'Test quote'})
+    quote((quoteData, err) => {
+        if (err) {
+            res.render("index", { quote: err });
+        }
+        if (quoteData && quoteData.status === 200) {
+            let quote = quoteData.data.en
+            res.render("index", { quote });
+        }
+        else {
+            res.render("index", {quote: "Could not fetch a quote. Try again?"})
+        }
+    })
 });
 
 app.listen(PORT)
