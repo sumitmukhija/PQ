d1c0384239537992224d205ddc10887a7500a1f3
sumit mukhija <sumukhija@gmail.com>
2020-05-27 13:46:04 +0100

    Add partial, link to Github

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require("path");
+const hbs = require('hbs');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')))
 app.set('view engine', 'hbs')
+hbs.registerPartials(path.join(__dirname, '../views/partials'))
 
 app.get('/', (req, res) => {
     res.render('index', {quote: 'Test quote'})
